8402e3619864df49ba21184ea510437644593e92
Doris Liu <doris.liuys@gmail.com>
2015-04-17 18:52:28 -0700

    Assignment 1

--- README.md
+++ README.md
@@ -2,4 +2,12 @@ COGS 121 Assignment 1 - Spring 2015
 ===========
 
 This is the starter code for an application that authenticates with Instagram via the Oauth protocol and Passport.
-Instructions are in the wiki.
+
+Name: Yusi Liu (Doris)
+PID: A97075340
+Partner's name: Rachel Rakuta
+PID: A10525182
+
+After logged in with Instagram, user can view their feeds.
+After logged in with Facebook, user can view their posts, photos and likes.
+
--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var passport = require('passport');
 var InstagramStrategy = require('passport-instagram').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var http = require('http');
 var path = require('path');
 var handlebars = require('express-handlebars');
@@ -10,20 +11,28 @@ var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var dotenv = require('dotenv');
 var Instagram = require('instagram-node-lib');
+var Facebook = require('fbgraph');
 var mongoose = require('mongoose');
 var app = express();
+var async = require('async');
 
 //local dependencies
 var models = require('./models');
 
 //client id and client secret here, taken from .env
 dotenv.load();
+
 var INSTAGRAM_CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID;
 var INSTAGRAM_CLIENT_SECRET = process.env.INSTAGRAM_CLIENT_SECRET;
 var INSTAGRAM_CALLBACK_URL = process.env.INSTAGRAM_CALLBACK_URL;
 var INSTAGRAM_ACCESS_TOKEN = "";
 Instagram.set('client_id', INSTAGRAM_CLIENT_ID);
 Instagram.set('client_secret', INSTAGRAM_CLIENT_SECRET);
+
+var FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
+var FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
+var FACEBOOK_CALLBACK_URL = process.env.FACEBOOK_CALLBACK_URL;
+var FACEBOOK_ACCESS_TOKEN = "";
 
 //connect to database
 mongoose.connect(process.env.MONGODB_CONNECTION_URL);
@@ -81,6 +90,37 @@ passport.use(new InstagramStrategy({
   }
 ));
 
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: FACEBOOK_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    models.User.findOrCreate({
+      "name": profile.username,
+      "id": profile.id,
+      "access_token": accessToken 
+    }, function(err, user, created) {
+      
+      // created will be true here
+      models.User.findOrCreate({}, function(err, user, created) {
+        // created will be false here
+        process.nextTick(function () {
+          // To keep the example simple, the user's Instagram profile is returned to
+          // represent the logged-in user.  In a typical application, you would want
+          // to associate the Instagram account with a user record in your database,
+          // and return that user instead.
+          return done(null, profile);
+        });
+      })
+    });
+  }
+));
+
+
+
 //Configures the Template engine
 app.engine('handlebars', handlebars({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
@@ -123,6 +163,8 @@ app.get('/account', ensureAuthenticated, function(req, res){
   res.render('account', {user: req.user});
 });
 
+
+
 app.get('/photos', ensureAuthenticated, function(req, res){
   var query  = models.User.where({ name: req.user.username });
   query.findOne(function (err, user) {
@@ -148,17 +190,291 @@ app.get('/photos', ensureAuthenticated, function(req, res){
 });
 
 
+app.get('/feed', ensureAuthenticated, function(req, res){
+  var query  = models.User.where({ name: req.user.username });
+  query.findOne(function (err, user) {
+    if (err) return handleError(err);
+    if (user) {
+      // doc may be null if no document matched
+      Instagram.users.self({
+        access_token: user.access_token,
+        complete: function(data) {
+          //Map will iterate through the returned data obj
+          var imageArr = data.map(function(item) {
+
+            //create temporary json object
+            tempJSON = {};
+            tempJSON.url = item.images.low_resolution.url;
+            
+            //get photo caption from image
+            if (item.caption) {
+              tempJSON.caption = item.caption.text;
+            } else {
+              tempJSON.caption = null;
+            }
+
+            tempJSON.likes = item.likes.count;
+            //console.log(item.likes.count);
+            tempJSON.tags = item.tags;
+
+            return tempJSON;
+          });
+          //console.log(imageArr);
+          res.render('feed', {feed: imageArr, user: req.user});
+        }
+      });
+    }
+  });
+});
+
+
+
+// Post instagram/like
+app.post('/feed', ensureAuthenticated, function(req, res){
+  var query = models.User.where({ name: req.user.username});
+  query.findOne(function (err, user) {
+    if (err) return handleError(err);
+      if (user) {
+        Instagram.media.likes({
+          access_token: user.access_token,
+          media_id: req.body.media_id,
+          complete: function(data) {
+            res.redirect('/feed');
+          }
+        });
+
+        Instagram.media.delete({
+          access_token: user.access_token,
+          media_id: req.body.media_id,
+          complete: function(data) {
+            res.redirect('/feed');
+          }
+        });
+
+
+      }
+    });
+});
+
+
+
+app.get('/fbaccount', ensureAuthenticated, function(req, res){
+  var query  = models.User.where({ id: req.user.id});
+  query.findOne(function (err, user) {
+    if (err) return handleError(err);
+    if (user) {
+      Facebook.setAccessToken(user.access_token);
+      
+      //get pictures from my posts
+      Facebook.get('/' + req.user.id, function(err, results){
+        //console.log(results);
+        var birthday = results.birthday;
+        var email = results.email;
+        var first_name = results.first_name;
+  
+        Facebook.get('/' + req.user.id + "/posts", function(err, results){
+        //console.log(results);
+        var data = results.data;
+        //console.log(data);
+          //Map will iterate through the returned data obj
+          var imageArr = data.map(function(item) {
+            //console.log(imageArr);
+            //create temporary json object
+            tempJSON = {};
+            tempJSON.url = item.picture;
+            
+            if (item.caption) {
+              tempJSON.caption = item.caption;
+            } else {
+              tempJSON.caption = null;
+            }
+            
+            tempJSON.time = item.created_time;
+
+            var date = new Date(item.created_time);
+            tempJSON.year = date.getFullYear();
+            tempJSON.month = date.getMonth() + 1;
+            tempJSON.day = date.getDate();
+
+
+            tempJSON.description = item.description;
+            tempJSON.iconurl = item.icon;
+            tempJSON.story = item.story;
+
+            tempJSON.pid = item.id;
+            //console.log(item.id);
+
+            //console.log('hi '+ results);
+
+            return tempJSON;
+            //console.log(results);
+          });
+
+
+          res.render('fbaccount', {fbaccount: imageArr, user: req.user, birthday: birthday, email: email, first_name: first_name});
+          //console.log(res);
+        }); //end GET /POSTS
+
+    });
+
+}
+});
+});
+
+
+      
+
+// GET FACEBOOK PHOTOS
+app.get('/facebook', ensureAuthenticated, function(req, res){
+  
+
+  var query  = models.User.where({ id: req.user.id});
+  query.findOne(function (err, user) {
+
+  var imageArr = [];
+    
+
+    if (err) return handleError(err);
+    if (user) {
+
+      Facebook.setAccessToken(user.access_token);
+
+      Facebook.get('/' + req.user.id, function(err, results){
+        //console.log(results);
+        var birthday = results.birthday;
+        var email = results.email;
+        var first_name = results.first_name;
+      
+      Facebook.get('/' + req.user.id + "/photos", function(err, results){
+    //console.log(results);
+        var data = results.data;
+      //console.log(data);
+        
+        var imageArr = data.map(function(item) {
+          //create temporary json object
+          
+              tempJSON = {};
+              tempJSON.url = item.source;
+          
+              if (item.caption) {
+                tempJSON.caption = item.caption;
+              } else {
+                tempJSON.caption = null;
+              }
+              
+              tempJSON.time = item.created_time;
+
+              var date = new Date(item.created_time);
+            tempJSON.year = date.getFullYear();
+            tempJSON.month = date.getMonth() + 1;
+            tempJSON.day = date.getDate();
+
+              tempJSON.description = item.name;
+              //console.log(item.name);
+             
+              Facebook.get('/' + item.id + '/likes?summary=1', function(err, ress) {
+                  tempJSON.likect = ress.summary.total_count;
+                  
+                  //imageArr.push(tempJSON.likect);
+                  //console.log('number of likes: '+ tempJSON.likect);
+             
+                  
+              //res.render('facebook', {user: req.user, facebook: imageArr});
+
+              }); //end GET LIKE COUNT
+              
+             // async.each(item, 
+               // function(item, callback){
+                 // item.
+               // }
+               return tempJSON;
+                  
+              
+        }); //end imageArr
+
+res.render('facebook', {user: req.user, facebook: imageArr, birthday: birthday, email: email, first_name: first_name});
+
+      }); // end GET PHOTOS
+});
+  }});
+});
+
+// GET FACEBOOK USER'S LIKED ITEMS
+app.get('/fblike', ensureAuthenticated, function(req, res){
+  var query  = models.User.where({ id: req.user.id});
+  query.findOne(function (err, user) {
+    if (err) return handleError(err);
+    if (user) {
+      Facebook.setAccessToken(user.access_token);
+      Facebook.get('/' + req.user.id, function(err, results){
+        //console.log(results);
+        var birthday = results.birthday;
+        var email = results.email;
+        var first_name = results.first_name;
+      //get pictures from my posts
+      Facebook.get('/' + req.user.id + "/likes", function(err, results){
+       
+        var data = results.data;
+
+        var likeArr = data.map(function(item) {
+          tempJSON = {};
+          tempJSON.category = item.category;
+          tempJSON.name = item.name;
+          
+          tempJSON.created_time = item.created_time;
+          var date = new Date(item.created_time);
+            tempJSON.year = date.getFullYear();
+            tempJSON.month = date.getMonth() + 1;
+            tempJSON.day = date.getDate();
+            
+          tempJSON.id = item.id;
+
+         // console.log(item.category);
+         Facebook.get('/' + item.id + '?fields=cover', function(err, results){
+          
+         })
+
+          return tempJSON;
+
+        });
+
+
+        res.render('fblike', {user: req.user, fblike: likeArr, birthday: birthday, email: email, first_name: first_name});
+      });
+    });
+   }});
+});
+
 // GET /auth/instagram
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in Instagram authentication will involve
 //   redirecting the user to instagram.com.  After authorization, Instagram
 //   will redirect the user back to this application at /auth/instagram/callback
+
 app.get('/auth/instagram',
   passport.authenticate('instagram'),
   function(req, res){
     // The request will be redirected to Instagram for authentication, so this
     // function will not be called.
   });
+
+// GET /auth/facebook
+app.get('/auth/facebook',
+  passport.authenticate('facebook', {scope: ['email',
+                                             'read_stream',
+                                             'user_birthday',
+                                             'user_about_me',
+                                             'user_likes',
+                                             'user_posts',
+                                             'user_location',
+                                             'user_photos',
+                                             'user_hometown',
+                                             'user_status',
+                                             'user_friends',
+                                             'user_interests'
+                                             ]})
+);
+
 
 // GET /auth/instagram/callback
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -168,8 +484,16 @@ app.get('/auth/instagram',
 app.get('/auth/instagram/callback', 
   passport.authenticate('instagram', { failureRedirect: '/login'}),
   function(req, res) {
-    res.redirect('/account');
+    res.redirect('/feed');
   });
+
+// GET /auth/facebook/callback
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { failureRedirect: '/login'}),
+  function(req, res) {
+    res.redirect('/fbaccount');
+  });
+
 
 app.get('/logout', function(req, res){
   req.logout();
